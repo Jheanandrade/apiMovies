9943d8f0c129f8a187d888791d0dd6a4699f6e7d
Jhean Andrade <jheanvpandraded@gmail.com>
2021-07-13 13:26:28 -0300

    feat: :sparkles: logical implementation of the methods used in the controller

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,9 +1,10 @@
-import { Movies, MoviesDocument } from './entities/movie.entity';
+import { Movies, MoviesDocument } from './database/entities/movie.entity';
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { MovieModel } from './models/movie';
 
 @Injectable()
 export class MoviesService {
@@ -12,24 +13,33 @@ export class MoviesService {
   ) {}
 
   create(createMovieDto: CreateMovieDto) {
-    const movie = new this.moviesModel(createMovieDto);
+    const movie = new this.moviesModel({ ...createMovieDto, favorite: true });
     return movie.save();
   }
 
-  findAll() {
-    return this.moviesModel.find();
-  }
-
-  findOne(id: string) {
-    return this.moviesModel.findById(id);
-  }
-
-  update(id: string, updateMovieDto: UpdateMovieDto) {
-    return this.moviesModel.findByIdAndUpdate(
-      { _id: id },
-      { $set: updateMovieDto },
-      { new: true },
+  async findAll(movie: string): Promise<MovieModel[]> {
+    const { data } = await axios.get(
+      `http://www.omdbapi.com/?apikey=925eba28&s=${movie}`,
     );
+
+    const mapFavorites = new Map();
+
+    const favorites = await this.find();
+
+    favorites.forEach((favorite) => {
+      mapFavorites.set(favorite.imdbID, true);
+    });
+
+    return data.Search.map((movie) => ({
+      title: movie.Title,
+      imdbID: movie.imdbID,
+      year: movie.Year,
+      favorite: mapFavorites.has(movie.imdbID),
+    }));
+  }
+
+  find() {
+    return this.moviesModel.find();
   }
 
   remove(id: string) {
