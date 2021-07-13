fd7034aa53f0d2ab1d04a2be9bc976c70e9a3972
Jhean Andrade <jheanvpandraded@gmail.com>
2021-07-13 13:22:46 -0300

    feat: :sparkles: implementation of code handling

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -3,35 +3,91 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  HttpException,
+  Res,
+  Query,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
-import { UpdateMovieDto } from './dto/update-movie.dto';
+import { HttpStatus } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
-  @Post()
-  create(@Body() createMovieDto: CreateMovieDto) {
-    return this.moviesService.create(createMovieDto);
+  @Post('/favorites')
+  async create(@Body() createMovieDto: CreateMovieDto, @Res() res) {
+    try {
+      const favoriteMovies = await this.moviesService.create(createMovieDto);
+
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'Your request processed with success',
+        data: favoriteMovies,
+      });
+    } catch (error) {
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Get()
-  findAll() {
-    return this.moviesService.findAll();
+  async findAll(@Res() res, @Query('movie') movie: string) {
+    try {
+      const getAll = await this.moviesService.findAll(movie);
+
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'Your request processed with success',
+        data: getAll,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.moviesService.findOne(id);
+  @Get('/favorites')
+  async getFavorites(@Res() res) {
+    try {
+      const getOne = await this.moviesService.find();
+
+      console.log(getOne);
+
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'Your request processed with success',
+        data: getOne,
+      });
+    } catch (error) {
+      throw new HttpException(
+        error.toString(),
+        error.getStatus() || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.moviesService.remove(id);
+  @Delete('/favorites/:id')
+  async remove(@Res() res, @Param('id') id: string) {
+    try {
+      await this.moviesService.remove(id);
+
+      return res.status(HttpStatus.OK).json({
+        statusCode: HttpStatus.OK,
+        message: 'Your request processed with success',
+      });
+    } catch (error) {
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
