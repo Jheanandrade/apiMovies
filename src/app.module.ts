0e09c2a2d91cf2575378b8a8301a0549b38253c6
Jhean Andrade <jheanvpandraded@gmail.com>
2021-07-14 20:10:45 -0300

    chore: :sparkles: ConfigModule.forRoot implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       'mongodb+srv://apiMovies:apiMovies@cluster0.xwzxz.mongodb.net/test',
     ),
